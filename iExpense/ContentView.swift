5ab60f5c743074efc0d10c1c36a76dc069f70d1f
Valcasara Bryan <31510683+ValcasaraBryan@users.noreply.github.com>
2024-06-07 11:35:49 +0200

    sheed / dismiss sheet

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -4,25 +4,29 @@
 //
 //  Created by valcasara-bryan on 07/06/2024.
 //
-import Observation
 import SwiftUI
 
-@Observable
-class User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    let name: String
+
+    var body: some View {
+        Button("Dismiss") {
+            dismiss()
+        }
+    }
 }
 
 struct ContentView: View {
-    @State private var user = User()
+    @State private var showingSheet = false
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        Button("Show Sheet") {
+            showingSheet.toggle()
         }
-        .padding()
+        .sheet(isPresented: $showingSheet, content: {
+            SecondView(name: "@twostraws")
+        })
     }
 }
 
