78da921476b662e6954688e0279a290232da2e7d
Valcasara Bryan <31510683+ValcasaraBryan@users.noreply.github.com>
2024-06-07 11:24:20 +0200

    first commit

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -4,16 +4,23 @@
 //
 //  Created by valcasara-bryan on 07/06/2024.
 //
-
+import Observation
 import SwiftUI
 
+@Observable
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
 struct ContentView: View {
+    @State private var user = User()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
         }
         .padding()
     }
