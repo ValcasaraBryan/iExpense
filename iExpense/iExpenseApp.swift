3d41decab30c14e0a09f0a6e4aa3ba56ca25c67b
Valcasara Bryan <31510683+ValcasaraBryan@users.noreply.github.com>
2024-06-07 11:10:11 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by valcasara-bryan on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
