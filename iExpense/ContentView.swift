ac022c284bed9fd36f08f6e4ea85b536ac53db17
Valcasara Bryan <31510683+ValcasaraBryan@users.noreply.github.com>
2024-06-07 15:28:03 +0200

    finished project

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,15 +7,70 @@
 
 import SwiftUI
 
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
+}
+
 struct ContentView: View {
-    //@State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
-    @AppStorage("tapCount") private var tapCount = 0
+    @State private var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
     
     var body: some View {
-        Button("Tap Count: \(tapCount)") {
-            tapCount += 1
-            //UserDefaults.standard.set(tapCount, forKey: "Tap")
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
+                }
+                .onDelete(perform: removeItem)
+            }
+            .navigationTitle("iExpenses")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    showingAddExpense = true
+                }
+                
+            }
+            .sheet(isPresented: $showingAddExpense, content: {
+                AddView(expenses: expenses)
+            })
         }
+    }
+    
+    func removeItem(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
